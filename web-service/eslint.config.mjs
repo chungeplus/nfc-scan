aa249e1e1972206f647fad695440de0260968a82
chungeplus <chungeplus@users.noreply.github.com>
2026-05-16 18:20:25 +0800

    build: align web service eslint with existing code style

--- web-service/eslint.config.mjs
+++ web-service/eslint.config.mjs
@@ -14,6 +14,18 @@ export default antfu(
     files: ['src/**/*.ts', 'tools/**/*.mjs'],
     rules: {
       'no-console': 'off',
+      'node/prefer-global/process': 'off',
+      'style/member-delimiter-style': ['error', {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      }],
+      'style/semi': ['error', 'always'],
       'ts/no-misused-promises': 'off',
     },
   },
